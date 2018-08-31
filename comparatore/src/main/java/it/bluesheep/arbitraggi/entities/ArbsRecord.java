71ee11defe870ae8eab15acdb2e469261205565e
giorgio <giorgio@mbp-di-giorgio.lan>
2018-08-31 18:55:03 +0200

    GD - 31/08/2018 - Commit nuove classi

---
+++ comparatore/src/main/java/it/bluesheep/arbitraggi/entities/ArbsRecord.java
@@ -0,0 +1,98 @@
+package it.bluesheep.arbitraggi.entities;
+
+import java.util.Date;
+
+public abstract class ArbsRecord{
+	
+	protected String bookmaker1;
+	protected String bookmaker2;
+	protected double odd1;
+	protected double odd2;
+	protected String bet1;
+	protected String bet2;
+	protected boolean betterOdd;
+	protected Date date;
+	protected String keyEvento;
+	protected String championship;
+	protected String sport;
+	protected double netProfit;
+	protected String link1;
+	protected String link2;
+	protected String country;
+	
+	protected abstract void calculateNetProfit();
+	
+	public abstract String getKeyEventoBookmakerBet();
+	
+	public abstract String getBookmakerList();
+
+	public static boolean isSameEventBookmakerBet(ArbsRecord record1, ArbsRecord record2) {
+		String uniqueKeyRecord1 = record1.getKeyEventoBookmakerBet();
+		String uniqueKeyRecord2 = record2.getKeyEventoBookmakerBet();
+		return uniqueKeyRecord1 != null && uniqueKeyRecord2 != null && uniqueKeyRecord2.equals(uniqueKeyRecord1);
+	}
+	
+	public boolean isBetterOdd() {
+		return betterOdd;
+	}
+
+	public void setBetterOdd(boolean betterOdd) {
+		this.betterOdd = betterOdd;
+	}
+
+	public String getKeyEvento() {
+		return keyEvento;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public String getChampionship() {
+		return championship;
+	}
+
+	public String getSport() {
+		return sport;
+	}
+	
+	public String getBookmaker1() {
+		return bookmaker1;
+	}
+
+	public String getBookmaker2() {
+		return bookmaker2;
+	}
+	
+	public double getOdd1() {
+		return odd1;
+	}
+
+	public double getOdd2() {
+		return odd2;
+	}
+	
+	public String getBet1() {
+		return bet1;
+	}
+
+	public String getBet2() {
+		return bet2;
+	}
+	
+	public double getNetProfit() {
+		return netProfit;
+	}
+
+	public String getLink1() {
+		return link1;
+	}
+
+	public String getLink2() {
+		return link2;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+}
---
+++ comparatore/src/main/java/it/bluesheep/arbitraggi/entities/TwoOptionsArbsRecord.java
@@ -0,0 +1,74 @@
+package it.bluesheep.arbitraggi.entities;
+
+import java.text.DecimalFormat;
+import java.util.Date;
+
+import it.bluesheep.util.BlueSheepConstants;
+
+public class TwoOptionsArbsRecord extends ArbsRecord {
+	
+	private double rating1;
+	private double rating2;
+	
+	public TwoOptionsArbsRecord(String keyEvento, Date date, String sport, String championship, String country, String bookmaker1, String bookmaker2, double odd1, double odd2, String bet1, String bet2, double rating1, double rating2, String link1, String link2) {
+		this.keyEvento = keyEvento;
+		this.date = date;
+		this.sport = sport;
+		this.championship = championship;
+		this.country = country;
+		this.bet1 = bet1;
+		this.bet2 = bet2;
+		this.bookmaker1 = bookmaker1;
+		this.bookmaker2 = bookmaker2;
+		this.odd1 = odd1;
+		this.odd2 = odd2;
+		this.rating1 = rating1;
+		this.rating2 = rating2;
+		this.link1 = link1;
+		this.link2 = link2;
+		betterOdd = false;
+		calculateNetProfit();
+	}
+	
+	@Override
+	protected void calculateNetProfit() {
+		DecimalFormat df = new DecimalFormat();
+		df.setMinimumFractionDigits(2);
+		df.setMaximumFractionDigits(2);		
+		
+		if (bet1.equals(bet2)) {
+			double realOdd2 = 1 / (1 - (1/odd2));
+			double r1 = 1000 * odd1;
+			double x = r1/realOdd2;
+			netProfit = (((r1)/(1000 + x)) - 1) * 100;			
+		} else {
+			double r1 = 1000 * odd1;
+			double x = r1/odd2;
+			netProfit = (((r1)/(1000 + x)) - 1) * 100;			
+		}
+	}
+	
+	public double getRating1() {
+		return rating1;
+	}
+
+	public double getRating2() {
+		return rating2;
+	}
+
+	@Override
+	public String getKeyEventoBookmakerBet() {
+		return keyEvento + BlueSheepConstants.REGEX_CSV + 
+			   championship + BlueSheepConstants.REGEX_CSV + 
+			   date + BlueSheepConstants.REGEX_CSV + 
+			   bookmaker1 + BlueSheepConstants.REGEX_CSV + 
+			   bookmaker2 + BlueSheepConstants.REGEX_CSV + 
+			   odd1 + BlueSheepConstants.REGEX_CSV +
+			   odd2 + BlueSheepConstants.REGEX_CSV;
+	}
+
+	@Override
+	public String getBookmakerList() {
+		return bookmaker1 + BlueSheepConstants.REGEX_CSV + bookmaker2;
+	}
+}
